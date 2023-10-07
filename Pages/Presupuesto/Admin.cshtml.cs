4f1115dd144aae5ed4f4eafef5e0e77f5f2ef32c
Porti <julian.portillo@outlook.com.ar>
2023-10-07 02:12:30 -0300

    Add the admin page and the basic structure for the 'Proveedores' section

---
+++ Pages/Presupuesto/Admin.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EstimadorBM.Pages.Presupuesto
+{
+    public class AdminModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
