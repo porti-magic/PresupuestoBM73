3f51af1685ca91412ec8b478f8a582b0f85eabdb
Porti <julian.portillo@outlook.com.ar>
2023-10-30 21:36:39 -0300

    Add the ingridient name, the 'isNew' property and the 'ToParam' method

--- Models/Precio.cs
+++ Models/Precio.cs
@@ -1,23 +1,34 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.Extensions.Hosting;
+using System.ComponentModel.DataAnnotations;
 
 namespace EstimadorBM.Models
 {
 	public class Precio
 	{
 		[Key]
-		public int IngredienteId { get; set; }
+		public string IngredienteName { get; set; }
 		public int ProveedorId { get; set; }
 		public string Marca { get; set; }
 		public int Presentacion { get; set; }
 		public decimal Costo { get; set; }
+		public bool IsNew { get; set; }
 
-        public Precio(int IngredienteId, int ProveedorId,string Marca, int Presentacion, decimal Costo)
-        {
-            this.IngredienteId = IngredienteId;
+		public Precio(string IngredienteName, int ProveedorId, string Marca, int Presentacion, decimal Costo, bool isNew)
+		{
+			this.IngredienteName = IngredienteName;
 			this.ProveedorId = ProveedorId;
 			this.Marca = Marca;
 			this.Presentacion = Presentacion;
 			this.Costo = Math.Round(Costo, 2);
-        }
-    }
+			IsNew = isNew;
+		}
+
+		public Dictionary<string, string> ToParam() => new()
+			{{"@IngredienteName", IngredienteName },
+				{"@ProveedorId", ProveedorId.ToString()},
+				{"@Marca", Marca},
+				{"@Presentacion", Presentacion.ToString()},
+				{"@Costo", Costo.ToString()},
+				{"@IsNew", IsNew.ToString()}};
+	}
 }
