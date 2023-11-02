bc6c43df568d26fdbf6a40518ef5e60c130a0b03
Porti <julian.portillo@outlook.com.ar>
2023-11-02 15:42:49 -0300

    Create the 'AdvanceSelector' class

---
+++ wwwroot/js/Elements/AdvanceSelector.js
@@ -0,0 +1,39 @@
+﻿window.addEventListener('load', function () {
+    const AdvanceselectorContainers = document.getElementsByClassName('AdvanceSelector-container');
+
+    for (let selectorContainer of AdvanceselectorContainers) {
+        let selector = NewAdvancedSelector(selectorContainer);
+        selector.ToggleBtn.onclick = function () { SwitchInputMethod(selector) };
+        selector.DropdownSelector.onclick = function () { AsignSelectorValue(selector); }
+    }
+});
+
+
+function SwitchInputMethod(selector)
+{
+    if (selector.ToggleBtn.checked) {
+        selector.CustomInput.removeAttribute("hidden");
+        selector.DropdownSelector.setAttribute("hidden", "");
+    }
+    else {
+        selector.CustomInput.setAttribute("hidden", "");
+        selector.DropdownSelector.removeAttribute("hidden");
+        AsignSelectorValue(selector);
+    }
+}
+
+function AsignSelectorValue(selector)
+{
+    selector.CustomInput.value = selector.DropdownSelector.value;
+}
+
+function NewAdvancedSelector(AdvanceselectorContainer)
+{
+    let AdvanceSelector = {};
+
+    AdvanceSelector.ToggleBtn = AdvanceselectorContainer.getElementsByClassName("CustomInputToggle")[0];
+    AdvanceSelector.CustomInput = AdvanceselectorContainer.getElementsByClassName("AdvanceSelector-customInput")[0];
+    AdvanceSelector.DropdownSelector = AdvanceselectorContainer.getElementsByClassName("AdvanceSelector-selector")[0];
+
+    return AdvanceSelector;
+}
