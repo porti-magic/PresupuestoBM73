a9043aeed1f6b55dfbf33ea2e01dfb9a4c691f44
Porti <julian.portillo@outlook.com.ar>
2023-11-03 00:52:03 -0300

    Create the 'AddIngrediente' functionality

--- wwwroot/js/Presupuesto/Admin.js
+++ wwwroot/js/Presupuesto/Admin.js
@@ -1,4 +1,4 @@
-﻿let ProveedoresTABLE, crearProveedorBTN;
+﻿import { SetUp as AdvanceSelectorSetUp } from "../Elements/AdvanceSelector.js";
 
 function SaveNewProveedor() {
     let form = document.getElementById("nuevoProveedorData");
@@ -21,9 +21,23 @@ function SaveNewProveedor() {
     }
 }
 
+function AddIngrediente()
+{
+    var ingridientRows = document.getElementsByClassName("IngredienteRow");
+    var lastIngridientRow = ingridientRows[ingridientRows.length - 1];
+    var newRow = lastIngridientRow.cloneNode(true);
+
+    AdvanceSelectorSetUp(newRow.getElementsByClassName("AdvanceSelector-container")[0])
+
+    lastIngridientRow.insertAdjacentElement('afterend', newRow);
+}
+
 window.onload = function () {
+    let ProveedoresTABLE, crearProveedorBTN, AgregarIngredienteBtn;
     ProveedoresTABLE = document.getElementById("proveedoresTable");
     crearProveedorBTN = document.getElementById("crearProveedorBtn");
+    AgregarIngredienteBtn = document.getElementById("agregarIngredienteoBtn");
 
     crearProveedorBTN.onclick = function () { SaveNewProveedor(); }
+    AgregarIngredienteBtn.addEventListener("click", AddIngrediente);
 }
