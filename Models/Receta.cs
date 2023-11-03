de40060543435fc84c20279e7d3d3135fb2cbffb
Porti <julian.portillo@outlook.com.ar>
2023-11-03 15:33:20 -0300

    Create 'Receta' model

---
+++ Models/Receta.cs
@@ -0,0 +1,10 @@
+﻿namespace EstimadorBM.Models
+{
+	public class Receta
+	{
+		public required string TragoNombre { get; set; }
+		public required bool Disponible { get; set;}
+		public required string[] IngredientesNombres { get; set; }
+		public required decimal[] IngredientesCantidades { get; set; }
+	}
+}
